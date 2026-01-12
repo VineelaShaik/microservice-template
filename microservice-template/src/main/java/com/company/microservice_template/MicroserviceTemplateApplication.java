4efb5de88c5bc6b00e1c59cc2450761df5133e28
Vineela Shaik <vineela.shaik@psiog.com>
2026-01-12 14:21:56 +0530

    Initial Spring Boot microservice template

---
+++ microservice-template/src/main/java/com/company/microservice_template/MicroserviceTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.company.microservice_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTemplateApplication.class, args);
+	}
+
+}
